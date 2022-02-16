4848db196d64c668d507143ad72df1df0ac07921
Csaba Tóth <tothcsa@gmail.com>
2022-02-16 11:53:50 +0100

    fix for cookie policy

--- pages/globalPage.js
+++ pages/globalPage.js
@@ -8,11 +8,11 @@ module.exports = {
     cookiesRequiredOnly: '#truste-consent-required'
   },
 
-  async acceptCookies (allCookies = false) {
-    const selector = allCookies ? this.selectors.cookiesAgree : this.selectors.cookiesAgree
+  async acceptCookies (allCookies = true) {
+    const selector = allCookies ? this.selectors.cookiesAgree : this.selectors.cookiesRequiredOnly
     I.waitForVisible(selector)
     I.click(selector)
-    I.waitForInvisible(this.selectors.cookiesModal)
+    I.waitForInvisible(this.selectors.cookiesModal, 60)
   },
 
   async waitForTimeout (n = 5000) {
